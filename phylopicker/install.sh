9deccd4a0397e2a1ef7afef4c7be6b5e9d43cac0
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 18:19:36 +0900

    edit phylopicker/install.sh

--- phylopicker/install.sh
+++ phylopicker/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-echo "Hello, World"
-# curl -sL https://github.com/alpineyahoo/dev/raw/main/phylopicker/download.sh >> phylopicker
-# chmod 700 phylopicker
+curl -sL https://github.com/alpineyahoo/dev/raw/main/phylopicker/download.sh >> phylopicker
+chmod 700 phylopicker
 echo "Executable \"phylopicker\" is downloaded in current directory.\nMove it to desired location, e.g. /usr/local/bin/phylopicker"
