036b3f5c6faa9a9715c377fc6d64018593cc0de2
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-29 01:03:08 +0900

    edit jokes/run.sh

--- jokes/run.sh
+++ jokes/run.sh
@@ -8,6 +8,9 @@ cat <<- EOF
 	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011514_00000_A_001.m4a" # Pigeon
 	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011518_00000_A_001.m4a" # School bell
 	deno run https://examples.deno.land/hello-world.ts # Hello, World!
+	curl 'wttr.in/Matsumoto?lang=ja&format=4' # Weather in Matsumoto, Japan
+	curl 'wttr.in/Matsumoto?lang=ja'
+	curl ja.wttr.in/Matsumoto
 EOF
 )
 echo $jokes | gum choose --limit=1 | zsh
