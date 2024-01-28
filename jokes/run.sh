69e04644dc6840b7e9b80337c478214980163a6b
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-29 00:59:00 +0900

    edit jokes/run.sh

--- jokes/run.sh
+++ jokes/run.sh
@@ -6,8 +6,8 @@ export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
 jokes=$(
 cat <<- EOF
 	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011514_00000_A_001.m4a" # Pigeon
-	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011518_00000_A_001.m4a"
-	deno run https://examples.deno.land/hello-world.ts
+	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011518_00000_A_001.m4a" # School bell
+	deno run https://examples.deno.land/hello-world.ts # Hello, World!
 EOF
 )
 echo $jokes | gum choose --limit=1 | zsh
