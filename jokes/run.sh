e0bb2c307b4ab694be51b2fbc264f9ec7c99e14c
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-29 00:53:41 +0900

    edit jokes/run.sh

--- jokes/run.sh
+++ jokes/run.sh
@@ -2,4 +2,12 @@
 
 # This is the "jokes" script written by @alpineyahoo
 
-echo "Hi, how are you?"
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
+jokes=$(
+cat <<- EOF
+	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011514_00000_A_001.m4a" # Pigeon
+	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011518_00000_A_001.m4a"
+	deno run https://examples.deno.land/hello-world.ts
+EOF
+)
+echo $jokes | gum choose --limit=1 | zsh
