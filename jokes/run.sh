cc6d473313da48abba2e9c264ab02e48605a9f87
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-09-16 22:15:10 +0900

    edit jokes/run.sh

--- jokes/run.sh
+++ jokes/run.sh
@@ -3,7 +3,7 @@
 # This is the "jokes" script written by @alpineyahoo
 
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
-jokes='$(
+jokes=$(
 cat <<- EOF
 	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011514_00000_A_001.m4a" # Pigeon
 	mpv --loop "https://www.nhk.or.jp/das/audio/D0002011/D0002011518_00000_A_001.m4a" # School bell
@@ -13,11 +13,11 @@ cat <<- EOF
 	curl ja.wttr.in/Matsumoto
 	fortune -f # list fortunes path
 	fortune
-	curl -sLI httpbin.org/status/200 -o /dev/null -w '%{http_code}\\n' # 200
-	curl -sLI httpbin.org/status/404 -o /dev/null -w '%{http_code}\\n' # 404
-	curl -sLI httpbin.org/status/418 -o /dev/null -w '%{http_code}\\n' # 418
-	curl -sLI httpie.io/hello -o /dev/null -w '%{http_code}\\n' # 200
-	curl -sLI example.com -o /dev/null -w '%{http_code}\\n' # 200
+	curl -sLI httpbin.org/status/200 -o /dev/null -w '%{http_code}' # 200
+	curl -sLI httpbin.org/status/404 -o /dev/null -w '%{http_code}' # 404
+	curl -sLI httpbin.org/status/418 -o /dev/null -w '%{http_code}' # 418
+	curl -sLI httpie.io/hello -o /dev/null -w '%{http_code}' # 200
+	curl -sLI example.com -o /dev/null -w '%{http_code}' # 200
 EOF
-)'
+)
 echo $jokes | gum choose --limit=1 | zsh
